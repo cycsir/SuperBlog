b1267cba35fb7c6e2d6eafcd16d0816ead0ecb0f
191250012 <191250012@smail.nju.edu.cn>
2022-05-31 10:03:15 +0800

    init

---
+++ src/main/java/com/example/superblog/SuperBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.superblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/superblog/SuperBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.superblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
